571ee32c911e95cd50b8fb2cb4916ac09dfd9dea
DerinDays <103086369+DerinDays@users.noreply.github.com>
2022-04-05 20:08:48 -0700

    Create 20220406_Variable.cpp

---
+++ 20220406_Variable.cpp
@@ -0,0 +1,14 @@
+//header
+#include <iostream>
+using namespace std;
+int main() {//begin of function
+float angka;
+string nama; //deklarasi variabel
+nama = "Daryn Iman Pratama"; //masukan kedalam variabel
+angka = 10.21 + 3.14;
+
+cout<<nama<<endl; //menampilkan variabel
+cout<<angka;
+cout<<angka+3.14;
+
+}//end of function
